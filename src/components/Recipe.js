5ca51b005723082bb66e0e834ad7e6524ac79e8e
AleksaGrocic <agrocic@gmail.com>
2024-08-21 16:36:07 +0200

    Created basic components

---
+++ src/components/Recipe.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Recipe = ({ recipe }) => {
+  return (
+    <Link to={`/recipes/${recipe.id}`} className="recipe__item">
+      <div className="recipe__header">
+        <div className="recipe__image">
+          <img src={recipe.imageUrl} alt={recipe.name} />
+        </div>
+        <div className="recipe__details">
+          <p className="recipe_name">{recipe.name.substring(0, 15)}</p>
+          <p className="recipe_title">{recipe.category}</p>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default Recipe;
